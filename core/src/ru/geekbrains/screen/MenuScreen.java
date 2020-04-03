39a5753c4ec81b9ce9ecb283be2612657bb016a5
Vadim Rybakov <vadimio9217@gmail.com>
2020-04-03 13:46:31 +0300

    add music on MenuScreen, sound on MainShip shooting, automatic shoot

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -2,6 +2,7 @@ package ru.geekbrains.screen;
 
 import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
@@ -34,12 +35,15 @@ public class MenuScreen extends BaseScreen {
         this.game = game;
     }
 
+    private Music music;
+
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         atlas = new TextureAtlas(Gdx.files.internal("textures/menuAtlas.tpack"));
         initSprites();
+        musicSet();
     }
 
     @Override
@@ -54,6 +58,7 @@ public class MenuScreen extends BaseScreen {
         bg.dispose();
         atlas.dispose();
         super.dispose();
+        music.stop();
     }
 
     @Override
@@ -111,6 +116,12 @@ public class MenuScreen extends BaseScreen {
         buttonExit.draw(batch);
         buttonPlay.draw(batch);
         batch.end();
+    }
+
+    public void musicSet(){
+        music = Gdx.audio.newMusic(Gdx.files.internal("sounds/music.mp3"));
+        music.setVolume(0.5f);
+        music.play();
     }
 
 }
